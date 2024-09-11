07f81ffdccd52db2933b8069fa0193d25538cd67
anzuv <127877298+anzuv@users.noreply.github.com>
2024-09-11 18:32:35 +0300

    Navigation

--- App.js
+++ App.js
@@ -1,13 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Feather from '@expo/vector-icons/Feather';
 import Styles from './styles/Styles.js';
 
-export default function App() {
+function AddworkoutScreen() {
   return (
-    <View style={Styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Add workout</Text>
+      
     </View>
   );
 }
 
+function WorkoutlistScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>List of workouts</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen 
+        name="Add workout" 
+        component={AddworkoutScreen}
+        options={{tabBarIcon:()=> <FontAwesome5 name="swimmer" size={24} color="black" />}}
+        />
+
+        <Tab.Screen 
+        name="List of workouts" 
+        component={WorkoutlistScreen}
+        options={{tabBarIcon:()=> <Feather name="list" size={24} color="black" />}}
+        />
+
+        <Tab.Screen 
+        name="Settings" 
+        component={SettingsScreen}
+        options={{tabBarIcon:()=> <Feather name="settings" size={24} color="black" />}}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
